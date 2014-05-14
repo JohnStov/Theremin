dcaa7788de0e07774f7160730eb487355e4a319b
John Stovin <john.d.stovin@gmail.com>
2014-05-14 23:31:02 +0100

    Sound

--- Theremin/App.xaml.cs
+++ Theremin/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Windows;
 using Leap;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
 
 namespace Theremin
 {
@@ -10,6 +14,37 @@ namespace Theremin
     {
         private App()
         {
+//            var waveOut = new WaveOut(WaveCallbackInfo.NewWindow());
+            var waveOut = new DirectSoundOut(60);
+            
+            waveOut.Init(new StreamProvider(new Oscillator(44100, Math.Sin)));
+            waveOut.Play();
+        }
+    }
+
+    internal class StreamProvider : WaveProvider32
+    {
+        private readonly Oscillator osc;
+
+        public StreamProvider(Oscillator osc)
+            : base(44100, 1)
+        {
+            this.osc = osc;
+        }
+
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            if (osc != null)
+            {
+                var data = osc.Data.Select(x => (float) x).Take(sampleCount).ToArray();
+                int index = 0;
+                foreach (var sample in data)
+                {
+                    buffer[offset + index++] = sample;
+                }
+            }
+
+            return sampleCount;
         }
     }
 }
---
+++ Theremin/Oscillator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Controls.Primitives;
+
+namespace Theremin
+{
+    public class Oscillator
+    {
+        public IEnumerable<double> Data { get; private set; }
+
+        private double delta;
+        private double frequency = 440;
+        private double phase = 0.0;
+
+        public Oscillator(int sampleRate, Func<double, double> generator)
+        {
+            delta = 2.0*Math.PI*frequency/sampleRate;
+            Data = Samples(generator);
+        }
+
+        private IEnumerable<double> Samples(Func<double, double> generator)
+        {
+            while (true)
+            {
+                yield return generator(phase);
+                phase = (phase + delta)%(2.0*Math.PI);
+            }
+        }
+    }
+}
